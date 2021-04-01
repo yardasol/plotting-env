86d628db6acc02964883f1ce812fe55b9a02ced0
yardasol <yardasol@grinnell.edu>
2021-04-01 10:39:44 -0500

    Added itkwidgets to install

--- plotting-env.sh
+++ plotting-env.sh
@@ -6,5 +6,5 @@ conda activate plotting
 conda config --env --append channels conda-forge
 conda config --env --set pip_interop_enabled True
 
-conda install pandas numpy scipy matplotlib pyvista notebook pyyaml mayavi
+conda install pandas numpy scipy matplotlib pyvista notebook pyyaml mayavi itkwidgets
 
